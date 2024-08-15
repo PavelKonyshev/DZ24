7110bc2776b688e39de588b24e69c1d70223c380
konys <Konyshev_PV@bk.ru>
2024-08-16 00:31:23 +0300

    DZ25.2

--- materials/views.py
+++ materials/views.py
@@ -1,60 +1,95 @@
+from materials.paginators import CoursePaginator, LessonPaginator
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveAPIView,
                                      UpdateAPIView)
 from rest_framework.viewsets import ModelViewSet
 
-from materials.models import Course, Lesson
-from materials.serializers import (CourseDatailSerializer, CourseSerializer,
-                                   LessonSerializer)
+from materials.models import Course, Lesson, Subscription
+from materials.serializers import (CourseSerializer,
+                                   LessonSerializer, SubscriptionSerializer)
 from users.permissions import IsModerator, IsOwner
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import generics, status, viewsets
 
-class CourseViewSet(ModelViewSet):
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
     queryset = Course.objects.all()
-
-    def get_serializer_class(self):
-        if self.action == "retrieve":
-            return CourseDatailSerializer
-        return CourseSerializer
+    pagination_class = CoursePaginator
 
     def perform_create(self, serializer):
         course = serializer.save()
         course.owner = self.request.user
         course.save()
 
-class LessonCreateApiView(CreateAPIView):
-    queryset = Lesson.objects.all()
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = (
+            ~IsModerator,)  # ~ знак обозначает обратное действие. т.е. (Не права IsModerator)
+        elif self.action in ['update', 'retrieve', 'partial_update']:
+            self.permission_classes = (IsModerator | IsOwner,)
+        elif self.action == 'destroy':
+            self.permission_classes = (IsOwner | ~IsModerator,)  # т.е. только владелец может удалять курс
+        return super().get_permissions()
+
+class LessonCreateApiView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+    permission_classes = (~IsModerator, IsAuthenticated)
 
     def perform_create(self, serializer):
         lesson = serializer.save()
         lesson.owner = self.request.user
         lesson.save()
 
-    def get_permissions(self):
-        if self.action == 'create':
-            self.permission_classes = (
-            ~IsModerator,)
-        elif self.action in ['update', 'retrieve', 'partial_update']:
-            self.permission_classes = (IsModerator | IsOwner,)
-        elif self.action == 'destroy':
-            self.permission_classes = (IsOwner | ~IsModerator,)
-        return super().get_permissions()
-    
-class LessonListApiView(ListAPIView):
-    queryset = Lesson.objects.all()
+
+class LessonListApiView(generics.ListAPIView):
     serializer_class = LessonSerializer
-
-
-class LessonRetrieveApiView(RetrieveAPIView):
     queryset = Lesson.objects.all()
+    pagination_class = LessonPaginator
+
+
+class LessonRetrieveApiView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
-
-
-class LessonUpdateApiView(UpdateAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = (IsAuthenticated, IsModerator | IsOwner,)
+
+
+class LessonUpdateApiView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
-
-
-class LessonDestroyApiView(DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = (IsAuthenticated, IsModerator | IsOwner,)
+
+
+class LessonDestroyApiView(generics.DestroyAPIView):
     serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner | ~IsModerator)
+
+
+class SubscriptionCreateApiView(APIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+    def post(self, *args, **kwargs):
+        user = self.request.user
+        course_id = self.request.data.get('course')
+        course_item = get_object_or_404(Course, pk=course_id)
+        subs_item = Subscription.objects.filter(user=user, course=course_item)
+
+        if subs_item.exists():
+            subs_item.delete()
+            message = 'подписка удалена'
+        else:
+            Subscription.objects.create(user=user, course=course_item)
+            message = 'подписка добавлена'
+        return Response({"message": message}, status=status.HTTP_201_CREATED)
+
+
+class SubscriptionListApiView(generics.ListAPIView):
+    serializer_class = SubscriptionSerializer
+    queryset = Subscription.objects.all()
