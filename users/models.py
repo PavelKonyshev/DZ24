5665d864ae76696438b0d1d6689c8e2e2100d02b
konys <Konyshev_PV@bk.ru>
2024-08-24 22:34:50 +0300

    DZ26.1

--- users/models.py
+++ users/models.py
@@ -65,7 +65,8 @@ class Payment(models.Model):
     payment_type = models.CharField(
         max_length=50, choices=PAYMENT_CHOICES, verbose_name="тип оплаты"
     )
-
+    session_id = models.CharField(max_length=255, verbose_name='id сессии', **NULLABLE)
+    link = models.URLField(max_length=400, verbose_name='ссылка на оплату', **NULLABLE)
     class Meta:
         verbose_name = "оплата"
         verbose_name_plural = "оплаты"
